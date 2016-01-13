105ab60ae43dfbd961858818cf8fff16c6fa762c
Hooman Taherinia <hoomant@gmail.com>
2016-01-13 18:33:46 +0330

    Done

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,45 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Assignment for the week 3 of the R Programming course
+## makeCacheMatrix creates an inverse-caching matrix
+## cacheSolve tries to inverse the inverse-caching matrix
+## or returns the cached version of it.
 
-## Write a short comment describing this function
+## Construct a new type of matrix, storing its inverse
+## into its cache when calculated. Also, retrieves the
+## cached inverse, if not changed, and if calculated.
 
 makeCacheMatrix <- function(x = matrix()) {
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inv) m <<- inv
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 
 }
 
 
-## Write a short comment describing this function
+## Inverse of the matrix of type makeCacheMatrix would
+## be returned if possible, and if the det(x) is non zero.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                message("The matrix has its inverse done before; retreiving cached data...")
+                return(m)
+        }
+        data <- x$get()
+        if(det(data)!=0) {
+                m<-solve(data,...)
+                x$setinverse(m)
+                return(m)
+        } else {
+                stop("The determinant of the matrix is 0, so it is not inversible")
+        }
 }
+
